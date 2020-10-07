25341f468b28887caa304ef1b704a4892eca8a0e
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-07 14:34:46 -0300

    refactor(api): functions
    * add estrutura inicial functions
    
    * $templete_diretorio -> para criar diretório
    
    * cria diretório produto, transacao

---
+++ wp-content/themes/api/functions.php
@@ -0,0 +1,11 @@
+<!--
+add(api): functions
+refactor(api): functions   
+rm(api): functions
+-->
+<?php 
+
+$templete_diretorio = get_template_directory();
+require_once($templete_diretorio . "/custom-post-type/produto.php");
+require_once($templete_diretorio . "/custom-post-type/transacao.php");
+?>

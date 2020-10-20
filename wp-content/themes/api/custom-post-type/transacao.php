342ab518089e9c16af614cf75903155b677382a8
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-20 18:31:24 -0300

    refactor(custom-post-type): transacao

--- wp-content/themes/api/custom-post-type/transacao.php
+++ wp-content/themes/api/custom-post-type/transacao.php
@@ -11,7 +11,7 @@ function registrar_cpt_transacao(){
     'public' => true,
     'show_ui' => true,
     'capability_type' => 'post',
-    'rewrite' => array('slug' => 'transacao', 'with' => true),
+    'rewrite' => array('slug' => 'transacao', 'with_front' => true),
     'query_var' => true,
     'supports' => array('custom-fields', 'author', 'title'),
     'publicly_queryalbe' => true

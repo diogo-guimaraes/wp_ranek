35c6a01ec1c412cc65e340b1abfb34e63a64837c
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-22 13:09:23 -0300

    refactor(endpoints): usuario_get
    * add ->  $user_id = $user->ID;
    
     vai fazer a verificação se o usuário está logado
    
     * add $user_meta
    
     para buscar os atributos pelo id dentro do objeto
    
     * add  $response
    
     retorna os dados em array

--- wp-content/themes/api/endpoints/usuario_get.php
+++ wp-content/themes/api/endpoints/usuario_get.php
@@ -4,6 +4,24 @@
 // rm(endpoints): usuario_get
 function api_usuario_get($request)
 {
+  $user = wp_get_current_user();
+  $user_id = $user->ID;
+  if($user_id > 0){
+    $user_meta = get_user_meta($user_id);
+
+    $response = array(
+      "id" => $user->user_login,
+      "nome" => $user->display_name,
+      "email" => $user->user_email,
+      "cep" => $user_meta['cep'][0],
+      "numero" => $user_meta['numero'][0],
+      "rua" => $user_meta['rua'][0],
+      "bairro" => $user_meta['bairro'][0],
+      "cidade" => $user_meta['cidade'][0],
+      "estado" => $user_meta['estado'][0],
+    );
+  }
+
 
   return rest_ensure_response($response);
 

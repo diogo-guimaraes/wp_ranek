3824d3493235fc7db5843706b43249b9bcffed3a
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-22 12:24:34 -0300

    refactor(endpoints): usuario_get
    * corrige estrutura inicial

--- wp-content/themes/api/endpoints/usuario_get.php
+++ wp-content/themes/api/endpoints/usuario_get.php
@@ -2,21 +2,21 @@
 // add(endpoints): usuario_get
 // refactor(endpoints): usuario_get 
 // rm(endpoints): usuario_get
-function api_usuario_post($request)
+function api_usuario_get($request)
 {
 
   return rest_ensure_response($response);
 
 }
 
-function registrar_api_usuario_post() {
+function registrar_api_usuario_get() {
   register_rest_route('api', '/usuario', array(
     array(
       'methods' => WP_REST_Server::READABLE,
-      'callback' => 'api_usuario_post',
+      'callback' => 'api_usuario_get',
     ),
   ));
 }
-add_action('rest_api_init', 'registrar_api_usuario_post');
+add_action('rest_api_init', 'registrar_api_usuario_get');
 
 ?>

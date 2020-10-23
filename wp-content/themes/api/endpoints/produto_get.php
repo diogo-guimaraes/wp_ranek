1db2fcb04f27e8bac3fe67aec5260159786a2d94
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-23 10:03:29 -0300

    refactor(endpoints): produto_get
    * if ($post_id) -> verifica se existe id
    
    * add $post_meta  para retornar as informações pelo id
    
    * add $response -> retona as informações desejadas

--- wp-content/themes/api/endpoints/produto_get.php
+++ wp-content/themes/api/endpoints/produto_get.php
@@ -6,8 +6,33 @@ function api_produto_get($request)
 { 
   $slug = $request["slug"];
   $post_id = get_produto_id_by_slug($slug);
-  
-  return rest_ensure_response($slug);
+
+    if ($post_id) {
+      //todas as informações de preço
+      $post_meta = get_post_meta($post_id);
+      $images = get_attached_media('image', $post_id);
+      $images_array = null;
+      
+      $response = array(
+        "id" => $slug, 
+        "fotos" => $images_array,
+        "nome" => $post_meta['nome'][0],
+        "preco" => $post_meta['preco'][0],
+        "descricao" => $post_meta['descricao'][0],
+        "vendido" => $post_meta['vendido'][0],
+        "usuario_id" => $post_meta['usuario_id'][0],
+      );
+
+     
+      
+
+
+    } else {
+      $response = new WP_Error('naoexiste', 'Produto não encontrado.', array('status' => 404));
+    }
+    
+
+  return rest_ensure_response($response);
 }
 
 function registrar_api_produto_get() {

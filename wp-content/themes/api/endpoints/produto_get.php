bf6eb5978534207780559c7dd78a35657a2358a2
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-23 08:59:07 -0300

    add(endpoints): produto_get
    * estrutura inicial de produto_get

---
+++ wp-content/themes/api/endpoints/produto_get.php
@@ -0,0 +1,23 @@
+<?php
+// add(endpoints): produto_get
+// refactor(endpoints): produto_get   
+// rm(endpoints): produto_get
+function api_produto_get($request)
+{ 
+
+
+  return rest_ensure_response($response);
+}
+
+function registrar_api_produto_get() {
+  register_rest_route('api', '/produto', array(
+    array(
+      'methods' => WP_REST_Server::EDITABLE,
+      'callback' => 'api_produto_get',
+    ),
+  ));
+}
+
+add_action('rest_api_init', 'registrar_api_produto_get');
+
+?>

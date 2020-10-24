a7189d186f3a88762e00940c3e2d6b7b91a32381
Diogo Guimarães <diogoguimaraes.br@hotmail.com>
2020-10-24 17:49:03 -0300

    refactor(endpoints): produto_get
    * add function produtos_scheme
    
     o código foi serpada nessa função para ser reutilizado
    
     * add registrar_api_produtos_get
    
      rota para produtos

--- wp-content/themes/api/endpoints/produto_get.php
+++ wp-content/themes/api/endpoints/produto_get.php
@@ -2,46 +2,47 @@
 // add(endpoints): produto_get
 // refactor(endpoints): produto_get   
 // rm(endpoints): produto_get
-function api_produto_get($request)
-{ 
-  $slug = $request["slug"];
+
+function produtos_scheme($request){
+  
   $post_id = get_produto_id_by_slug($slug);
 
-    if ($post_id) {
-      // get_post_meta-> retorna todas as informações pelo id
-      $post_meta = get_post_meta($post_id);
-      $images = get_attached_media('image', $post_id);
-      $images_array = null;
+  if ($post_id) {
+    // get_post_meta-> retorna todas as informações pelo id
+    $post_meta = get_post_meta($post_id);
+    $images = get_attached_media('image', $post_id);
+    $images_array = null;
 
-      if($images) {
-        $images_array = array();
-        foreach($images as $key => $value) {
-          $images_array[] = array(
-            'titulo' => $value->post_name,
-            'src' => $value->guid,
-          );
-        }
+    if($images) {
+      $images_array = array();
+      foreach($images as $key => $value) {
+        $images_array[] = array(
+          'titulo' => $value->post_name,
+          'src' => $value->guid,
+        );
       }
-
-      $response = array(
-        "id" => $slug, 
-        "fotos" => $images_array,
-        "nome" => $post_meta['nome'][0],
-        "preco" => $post_meta['preco'][0],
-        "descricao" => $post_meta['descricao'][0],
-        "vendido" => $post_meta['vendido'][0],
-        "usuario_id" => $post_meta['usuario_id'][0],
-      );
-
-     
-      
-
-
-    } else {
-      $response = new WP_Error('naoexiste', 'Produto não encontrado.', array('status' => 404));
     }
-    
 
+    $response = array(
+      "id" => $slug, 
+      "fotos" => $images_array,
+      "nome" => $post_meta['nome'][0],
+      "preco" => $post_meta['preco'][0],
+      "descricao" => $post_meta['descricao'][0],
+      "vendido" => $post_meta['vendido'][0],
+      "usuario_id" => $post_meta['usuario_id'][0],
+    );
+
+
+  } else {
+    $response = new WP_Error('naoexiste', 'Produto não encontrado.', array('status' => 404));
+  }
+  return $response;
+}
+
+function api_produto_get($request)
+{ 
+  $response = produtos_scheme($request["slug"]);
   return rest_ensure_response($response);
 }
 
@@ -54,6 +55,22 @@ function registrar_api_produto_get() {
   ));
 }
 
+function registrar_api_produtos_get() {
+  register_rest_route('api', '/produto', array(
+    array(
+      'methods' => WP_REST_Server::READABLE,
+      'callback' => 'api_produtos_get',
+    ),
+  ));
+}
+
 add_action('rest_api_init', 'registrar_api_produto_get');
+
+function api_produtos_get($request) {
+
+
+
+}
+  
 
 ?>
